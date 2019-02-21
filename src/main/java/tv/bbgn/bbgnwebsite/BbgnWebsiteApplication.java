1c22902dff04900a6bf7ab7b00f2e085fa5e5a0b
Chad <ch.collins@outlook.com>
2019-02-21 16:26:38 -0500

    first commit

---
+++ src/main/java/tv/bbgn/bbgnwebsite/BbgnWebsiteApplication.java
@@ -0,0 +1,13 @@
+package tv.bbgn.bbgnwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbgnWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbgnWebsiteApplication.class, args);
+	}
+
+}
